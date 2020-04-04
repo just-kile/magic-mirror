f15c57516179c61fafccb55bc63fb50911a823d7
Paul Boeck <boeck87@gmail.com>
2020-04-04 16:11:45 +0200

    Add working ws connection

--- server/index.js
+++ server/index.js
@@ -4,10 +4,18 @@ const path = require('path');
 const { PythonShell } = require('python-shell');
 
 let app = express();
+require('express-ws')(app);
 
 const public = path.join(__dirname, 'public');
 
 app.use('/', express.static(public));
+
+app.ws('/echo', (ws, req) => {
+  console.log('New websocket request on echo');
+  setTimeout(() => ws.send('Message from server'), 100);
+
+  ws.on('message', msg => ws.send(msg));
+});
 
 const port = +process.env.PORT || 3000; //TODO load from env
 app.listen(port);
@@ -20,16 +28,16 @@ const pyOptions = {
   args: []
 };
 
-let pyshell = new PythonShell('../face-detection/face_detecter.py', pyOptions);
+// let pyshell = new PythonShell('../face-detection/face_detecter.py', pyOptions);
 
-pyshell.on('message', m => {
-  console.log('received', m)
-});
+// pyshell.on('message', m => {
+  // console.log('received', m)
+// });
 
-// end the input stream and allow the process to exit
-pyshell.end(function (err, code, signal) {
-  if (err) throw err;
-  console.log('The exit code was: ' + code);
-  console.log('The exit signal was: ' + signal);
-  console.log('finished');
-});
+// // end the input stream and allow the process to exit
+// pyshell.end(function (err, code, signal) {
+  // if (err) throw err;
+  // console.log('The exit code was: ' + code);
+  // console.log('The exit signal was: ' + signal);
+  // console.log('finished');
+// });
